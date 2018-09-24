50582b83bda3600544b53b3742c2dc37bc2988a1
Mario Weiss <mobil@ing-weiss.com>
2018-09-24 19:30:31 +0200

    add rotation effect

--- js/main.js
+++ js/main.js
@@ -7,4 +7,15 @@ function showTime() {
     var t = setTimeout(showTime, 1000);
 }
 
+
+function rotate(img, deg, duration) {
+    document.getElementById(img).style.transition = "all "+duration+"" ;
+    document.getElementById(img).style.transform = "rotate("+deg+"deg)";
+}
+
 showTime();
+rotate("martina", 180);
+rotate("martina1", 180);
+
+document.getElementById("martina").addEventListener("mouseover", function() {rotate("martina", 0, ".5s"); rotate("martina1", 0, ".5s")});
+document.getElementById("martina").addEventListener("mouseout", function(){rotate("martina", 180, "5s"); rotate("martina1", 180, "5s")});
