48fdd276a15d02dbe6676d6ea9840f06654b962c
Mario Weiss <mobil@ing-weiss.com>
2018-09-26 02:03:47 +0200

    add animation to main page

--- js/main.js
+++ js/main.js
@@ -11,11 +11,36 @@ function showTime() {
 function rotate(img, deg, duration) {
     document.getElementById(img).style.transition = "all "+duration+"" ;
     document.getElementById(img).style.transform = "rotate("+deg+"deg)";
+    setTimeout(restore,5000)
+}
+
+function restore (){
+    document.getElementById("martina").style.transition = "all .5s";
+    document.getElementById("martina1").style.transition = "all .5s";
 }
 
 showTime();
 rotate("martina", 180, ".1s");
 rotate("martina1", 180, ".1s");
+
+var anim_text = ["Martina is the Queen of useless facts!!!","Yamam is the King of muscles","Zsolt is our Nevermind-guy","Mario is the Super Mario Master"];
+var text = document.getElementById("anim_text")
+
+function textrotate() {
+    if (text.innerHTML == anim_text[0]) {
+        text.innerHTML = anim_text[1]
+    } else if (text.innerHTML == anim_text[1] ) {
+        text.innerHTML = anim_text[2]
+    } else if (text.innerHTML == anim_text[2]) { 
+        text.innerHTML = anim_text[3]
+    } else if (text.innerHTML == anim_text[3]) { 
+        text.innerHTML = anim_text[0]
+    }
+    setTimeout(textrotate, 8000);
+}
+
+text.innerHTML = anim_text[3];
+textrotate()
 
 document.getElementById("martina").addEventListener("mouseover", function() {rotate("martina", 0, ".5s"); rotate("martina1", 0, ".5s")});
 document.getElementById("martina").addEventListener("mouseout", function(){rotate("martina", 180, "5s"); rotate("martina1", 180, "5s")});
